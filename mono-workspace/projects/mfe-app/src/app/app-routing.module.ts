08187c7f604d5e55b46dccda2c8c5de127186855
Amar Kumar Ram <ram.amarkumar@gmail.com>
2023-05-01 20:14:02 +0530

    mfe app created

--- mono-workspace/projects/mfe-app/src/app/app-routing.module.ts
+++ mono-workspace/projects/mfe-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/todo-list', pathMatch: 'full'},
+  {path: 'todo-list', component: TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
