d6c621ca32e653736b195f99bf1b3316edaf7a6b
Amar Kumar Ram <ram.amarkumar@gmail.com>
2023-05-01 19:49:20 +0530

    host app created

--- mono-workspace/projects/host-app/src/app/app-routing.module.ts
+++ mono-workspace/projects/host-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'todo-list', component: TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routeCompArr = [HomeComponent, TodoComponent];
